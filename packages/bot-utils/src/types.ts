7031831417ebab05e8305ca3af445eb47491f57d
DecliningLotus <declininglotus@gmail.com>
2022-08-05 16:29:17 +0300

    feat(types): add proper typings

--- packages/bot-utils/src/types.ts
+++ packages/bot-utils/src/types.ts
@@ -1,22 +1,40 @@
 // types
-const ALLOWED_QUERY_TYPES = ['ping', 'traceroute', 'dns', 'mtr', 'http'] as const;
+export const ALLOWED_QUERY_TYPES = ['ping', 'traceroute', 'dns', 'mtr', 'http'] as const;
+export type QueryType = typeof ALLOWED_QUERY_TYPES[number];
+export const isQueryType = (type: string): type is QueryType => ALLOWED_QUERY_TYPES.includes(type as QueryType);
 
 // filters
-const ALLOWED_LOCATION_TYPES = ['continent', 'region', 'country', 'state', 'city', 'asn', 'network', 'magic'] as const;
+export const ALLOWED_LOCATION_TYPES = ['continent', 'region', 'country', 'state', 'city', 'asn', 'network', 'magic'] as const;
+export type LocationType = typeof ALLOWED_LOCATION_TYPES[number];
+export const isLocationType = (type: string): type is LocationType => ALLOWED_LOCATION_TYPES.includes(type as LocationType);
 
 // traceroute
-const ALLOWED_TRACE_PROTOCOLS = ['TCP', 'UDP', 'ICMP'] as const;
+export const ALLOWED_TRACE_PROTOCOLS = ['TCP', 'UDP', 'ICMP'] as const;
+export type TraceProtocol = typeof ALLOWED_TRACE_PROTOCOLS[number];
+export const isTraceProtocol = (type: string): type is TraceProtocol => ALLOWED_TRACE_PROTOCOLS.includes(type as TraceProtocol);
 
 // dns
-const ALLOWED_DNS_TYPES = ['A', 'AAAA', 'ANY', 'CNAME', 'DNSKEY', 'DS', 'MX', 'NS', 'NSEC', 'PTR', 'RRSIG', 'SOA', 'TXT', 'SRV'] as const;
-const ALLOWED_DNS_PROTOCOLS = ['UDP', 'TCP'] as const;
+export const ALLOWED_DNS_TYPES = ['A', 'AAAA', 'ANY', 'CNAME', 'DNSKEY', 'DS', 'MX', 'NS', 'NSEC', 'PTR', 'RRSIG', 'SOA', 'TXT', 'SRV'] as const;
+export type DnsType = typeof ALLOWED_DNS_TYPES[number];
+export const isDnsType = (type: string): type is DnsType => ALLOWED_DNS_TYPES.includes(type as DnsType);
+
+export const ALLOWED_DNS_PROTOCOLS = ['UDP', 'TCP'] as const;
+export type DnsProtocol = typeof ALLOWED_DNS_PROTOCOLS[number];
+export const isDnsProtocol = (type: string): type is DnsProtocol => ALLOWED_DNS_PROTOCOLS.includes(type as DnsProtocol);
 
 // mtr
-const ALLOWED_MTR_PROTOCOLS = ['TCP', 'UDP', 'ICMP'] as const;
+export const ALLOWED_MTR_PROTOCOLS = ['TCP', 'UDP', 'ICMP'] as const;
+export type MtrProtocol = typeof ALLOWED_MTR_PROTOCOLS[number];
+export const isMtrProtocol = (type: string): type is MtrProtocol => ALLOWED_MTR_PROTOCOLS.includes(type as MtrProtocol);
 
 // http
-const ALLOWED_HTTP_PROTOCOLS = ['HTTP', 'HTTPS', 'HTTP2'] as const;
-const ALLOWED_HTTP_METHODS = ['GET', 'HEAD'] as const;
+export const ALLOWED_HTTP_PROTOCOLS = ['HTTP', 'HTTPS', 'HTTP2'] as const;
+export type HttpProtocol = typeof ALLOWED_HTTP_PROTOCOLS[number];
+export const isHttpProtocol = (type: string): type is HttpProtocol => ALLOWED_HTTP_PROTOCOLS.includes(type as HttpProtocol);
+
+export const ALLOWED_HTTP_METHODS = ['GET', 'HEAD'] as const;
+export type HttpMethod = typeof ALLOWED_HTTP_METHODS[number];
+export const isHttpMethod = (type: string): type is HttpMethod => ALLOWED_HTTP_METHODS.includes(type as HttpMethod);
 
 interface Locations {
 	continent?: string;
@@ -29,26 +47,59 @@ interface Locations {
 	magic?: string;
 }
 
-export interface PostMeasurement {
+export interface SharedMeasurement {
+	target: string
 	limit: number
 	locations: Locations[]
-	type: string
-	target: string
-	measurementOptions: {
-		query?: {
-			type?: string
-		}
-		request?: {
-			headers?: Record<string, string>
-			path?: string
-			host?: string
-			query?: string
-			method?: string
-		}
-		protocol?: string
-		port?: number
-		resolver?: string
-		trace?: boolean
+}
+
+export interface PingMeasurement extends SharedMeasurement {
+	type: 'ping'
+	measurementOptions?: {
 		packets?: number
 	}
 }
+export interface TraceMeasurement extends SharedMeasurement {
+	type: 'traceroute'
+	measurementOptions?: {
+		protocol?: TraceProtocol
+		port?: number
+	}
+}
+
+export interface DnsMeasurement extends SharedMeasurement {
+	type: 'dns'
+	measurementOptions?: {
+		query?: {
+			type: DnsType
+		}
+		protocol?: DnsProtocol
+		port?: number
+		resolver?: string
+		trace?: boolean
+	}
+}
+
+export interface MtrMeasurement extends SharedMeasurement {
+	type: 'mtr'
+	measurementOptions?: {
+		protocol?: MtrProtocol
+		port?: number
+		packets?: number
+	}
+}
+
+export interface HttpMeasurement extends SharedMeasurement {
+	type: 'http'
+	port?: number
+	protocol?: HttpProtocol
+	request?: {
+		path?: string
+		query?: string
+		method?: HttpMethod
+		host?: string
+		headers?: Record<string, string>
+	}
+}
+
+export type PostMeasurement = PingMeasurement | TraceMeasurement | DnsMeasurement | MtrMeasurement | HttpMeasurement;
