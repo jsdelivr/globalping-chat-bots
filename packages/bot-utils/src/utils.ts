cfd8f6936e936ea3212103dbe65da8e46b59cba1
DecliningLotus <declininglotus@gmail.com>
2022-08-05 16:27:52 +0300

    feat: infer urls for http method

--- packages/bot-utils/src/utils.ts
+++ packages/bot-utils/src/utils.ts
@@ -1,15 +1,40 @@
+import type { Arguments } from 'yargs-parser';
 import parser from 'yargs-parser';
 
-export const parseArgs = (args: string[]) => {
+export const parseArgs = (argv: string | string[]) => {
+	let args = argv;
+	if (typeof args === 'string')
+		args = args.split(' ');
+
 	if (args.indexOf('from') === 2)
 		args[2] = '--from';
 
-	const results = parser(args, {
+	const parsed = parser(args, {
 		array: ['from', 'limit', 'packets', 'port', 'protocol', 'type', 'resolver', 'path', 'query', 'host', 'method', 'header'],
 		configuration: {
 			'greedy-arrays': true,
 		}
 	});
 
-	return results;
+	if (parsed._[0] === 'http') {
+		try {
+			const url = new URL(parsed._[1] as string);
+
+			// If a flag isn't passed to override, infer from URL obj instead
+			if (!parsed.host && url.hostname) parsed.host = [url.hostname];
+			if (!parsed.path && url.pathname) parsed.path = [url.pathname];
+			if (!parsed.port && url.port) parsed.port = [Number(url.port)];
+			if (!parsed.protocol && url.protocol) parsed.protocol = [url.protocol.replace(':', '')];
+			if (!parsed.query && url.search) parsed.query = [url.search];
+		} catch {
+			// Do nothing
+		}
+	}
+
+	return parsed;
+};
+
+export const validateArgs = (args: Arguments) => {
+
+
 };
--- packages/bot-utils/tests/utils.test.ts
+++ packages/bot-utils/tests/utils.test.ts
@@ -6,7 +6,7 @@ describe('Utils', () => {
 	describe('parseArgs', () => {
 		it('should parse args', () => {
 			const args = 'http google.com from New York --limit 2 --path / --query ?a=abc --host google.com --method get --port 80 --protocol https --resolver 0.0.0.0 --header Content-Encoding: gzip --header Content-Type: text/html; charset=utf-8';
-			const result = parseArgs(args.split(' '));
+			const result = parseArgs(args);
 
 			expect(result).toEqual({
 				_: ['http', 'google.com'],
@@ -22,6 +22,31 @@ describe('Utils', () => {
 				limit: [2],
 				method: ['get'],
 				path: ['/'],
+				port: [80],
+				protocol: ['https'],
+				query: ['?a=abc'],
+				resolver: ['0.0.0.0']
+			});
+		});
+
+		it('should infer url for http parse', () => {
+			const args = 'http https://google.com:80/test?a=abc from New York --limit 2 --method get --resolver 0.0.0.0 --header Content-Encoding: gzip --header Content-Type: text/html; charset=utf-8';
+			const result = parseArgs(args.split(' '));
+
+			expect(result).toEqual({
+				_: ['http', 'https://google.com:80/test?a=abc'],
+				from: ['New', 'York'],
+				header: [
+					'Content-Encoding:',
+					'gzip',
+					'Content-Type:',
+					'text/html;',
+					'charset=utf-8',
+				],
+				host: ['google.com'],
+				limit: [2],
+				method: ['get'],
+				path: ['/test'],
 				port: [80],
 				protocol: ['https'],
 				query: ['?a=abc'],
