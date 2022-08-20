e525378cbae5eb0272f3d56bc4c4ec9aa7516cb0
DecliningLotus <declininglotus@gmail.com>
2022-08-21 02:39:24 +0300

    fix(discord): add http error body to user error

--- packages/discord/src/app.ts
+++ packages/discord/src/app.ts
@@ -1,6 +1,7 @@
 import { getMeasurement, parseFlags, postMeasurement } from '@globalping/bot-utils/src/index';
 import { Client, codeBlock, GatewayIntentBits, inlineCode, userMention } from 'discord.js';
 import * as dotenv from 'dotenv';
+import { HTTPError } from 'got';
 
 import { logger } from './logger';
 import { expandFlags, expandResults, getFlags } from './utils';
@@ -40,8 +41,13 @@ client.on('interactionCreate', async interaction => {
 			await interaction.editReply(`${userMention(user.id)}, here are the results for ${inlineCode(`${flags.cmd} ${flags.target} from ${flags.from}${txtFlags}`)}`);
 			await expandResults(res, interaction);
 		} catch (error) {
+			let msg = error;
+			// Got does not expose the returned error message from the API by default
+			if (error instanceof HTTPError)
+				msg = `${error}\n${error.response.body}`;
+
 			await interaction.editReply(`${userMention(user.id)}, there was an error processing your request`);
-			await interaction.followUp({ content: codeBlock(String(error)), fetchReply: false });
+			await interaction.followUp({ content: codeBlock(msg), fetchReply: false });
 		}
 	}
 });
