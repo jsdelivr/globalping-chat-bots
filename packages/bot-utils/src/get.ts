5d4b5f47984b6ca19bbaa68bd01d9b3918c20899
Adil H <1284255+didil@users.noreply.github.com>
2023-04-14 11:28:37 +0100

    Implement api guidelines (#16)
    * implement api guidelines
    
    * api poll interval 1s

--- packages/bot-utils/src/get.ts
+++ packages/bot-utils/src/get.ts
@@ -1,22 +1,86 @@
 /* eslint-disable no-await-in-loop */
-import got from 'got';
+import got, { Headers, Response } from 'got';
 
 import type { MeasurementResponse } from './types';
+import { userAgent } from './user-agent';
 
-const fetchMeasurement = async (id: string): Promise<MeasurementResponse> => {
-	const res = await got.get(`https://api.globalping.io/v1/measurements/${id}`);
-	if (res.statusCode === 200)
-		return JSON.parse(res.body);
+class MeasurementsFetcher {
+	// The api url endpoint
+	apiUrl: string;
 
-	throw new Error(JSON.parse(res.body));
-};
+	// caches Etags by measurement id
+	etags: Record<string, string>;
+
+	// caches Measurements by ETag
+	measurements: Record<string, MeasurementResponse>;
+
+	constructor(apiUrl: string) {
+		this.apiUrl = apiUrl;
+		this.etags = {};
+		this.measurements = {};
+	}
+
+	async fetchMeasurement(id: string): Promise<MeasurementResponse> {
+		const headers: Headers = {
+			'User-Agent': userAgent(),
+			'Accept-Encoding': 'br',
+		};
+
+		let etag: string | undefined = this.etags[id];
+		if (etag !== undefined) {
+			headers['If-None-Match'] = etag;
+		}
+
+		const res: Response<string> = await got.get(`${this.apiUrl}/${id}`, {
+			method: 'GET',
+			headers,
+		});
+
+		if (res.statusCode === 304) {
+			// 304 not modified
+			// get response from cache
+			const measurementResponse = this.measurements[etag];
+			if (measurementResponse === undefined) {
+				throw new Error('response not found in etags cache');
+			}
+
+			return measurementResponse;
+		} if (res.statusCode >= 400) {
+			if (res.statusCode === 500) {
+				throw new Error(JSON.parse(res.body));
+			} else if (res.statusCode === 404) {
+				throw new Error('measurement not found');
+			} else {
+				throw new Error(`error with http status code: ${res.statusCode}`);
+			}
+		}
+
+		const measurementResponse: MeasurementResponse = JSON.parse(res.body);
+
+		// save etag and response to cache
+		etag = res.headers.etag;
+		if (etag !== undefined) {
+			this.etags[id] = etag;
+			this.measurements[etag] = measurementResponse;
+		}
+
+		return measurementResponse;
+	};
+}
+
+export const ApiUrl = 'https://api.globalping.io/v1/measurements';
+
+// api poll interval in milliseconds
+const apiPollInterval = 1000;
 
 export const getMeasurement = async (id: string): Promise<MeasurementResponse> => {
-	let data = await fetchMeasurement(id);
+	const measurementsFetcher = new MeasurementsFetcher(ApiUrl);
+
+	let data = await measurementsFetcher.fetchMeasurement(id);
 	while (data.status === 'in-progress') {
 		// eslint-disable-next-line no-promise-executor-return
-		await new Promise(resolve => setTimeout(resolve, 100));
-		data = await fetchMeasurement(id);
+		await new Promise(resolve => setTimeout(resolve, apiPollInterval));
+		data = await measurementsFetcher.fetchMeasurement(id);
 	}
 	return data;
 };
@@ -29,3 +93,5 @@ export const getTag = (tags: string[]): string | undefined => {
 	}
 	return undefined;
 };
+
+
