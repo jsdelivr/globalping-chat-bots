1fe7d23c823becdc065d5f5502d18976a665ad86
didil <1284255+didil@users.noreply.github.com>
2023-07-11 12:05:08 +0100

    /github-bot route

--- packages/slack/src/routes.ts
+++ packages/slack/src/routes.ts
@@ -1,8 +1,10 @@
-import { CustomRoute } from '@slack/bolt';
+import { BufferedIncomingMessage, CustomRoute, HTTPModuleFunctions } from '@slack/bolt';
 // import { client as discord } from 'discord-bot/src/app';
 import * as fs from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { logger } from './utils';
+import { v4 as uuidv4 } from 'uuid';
 
 import * as database from './db';
 // import { logger } from './utils';
@@ -12,6 +14,7 @@ export const routes: CustomRoute[] = [
 		path: '/health',
 		method: ['GET'],
 		handler: async (_req, res) => {
+
 			try {
 				// Check if db is accessible
 				await database.knex.raw('select 1+1 as result');
@@ -26,10 +29,12 @@ export const routes: CustomRoute[] = [
 				// logger.debug('Discord bot is accessible');
 
 				res.writeHead(200);
-				res.end('OK');
+				res.write('OK');
+				res.end();
 			} catch (error) {
 				res.writeHead(503);
-				res.end(error);
+				res.write(JSON.stringify(error));
+				res.end();
 			}
 		},
 	},
@@ -43,7 +48,8 @@ export const routes: CustomRoute[] = [
 				}).end();
 			} catch (error) {
 				res.writeHead(503);
-				res.end(error);
+				res.write(JSON.stringify(error));
+				res.end();
 			}
 		},
 	},
@@ -56,7 +62,8 @@ export const routes: CustomRoute[] = [
 				fs.createReadStream(path.join(path.dirname(fileURLToPath(import.meta.url)), '../public/favicon.ico')).pipe(res);
 			} catch (error) {
 				res.writeHead(503);
-				res.end(error);
+				res.write(JSON.stringify(error));
+				res.end();
 			}
 		},
 	},
@@ -69,8 +76,68 @@ export const routes: CustomRoute[] = [
 				fs.createReadStream(path.join(path.dirname(fileURLToPath(import.meta.url)), '../public/robots.txt')).pipe(res);
 			} catch (error) {
 				res.writeHead(503);
-				res.end(error);
+				res.write(JSON.stringify(error));
+				res.end();
 			}
 		}
-	}
+	},
+	{
+		path: '/github-bot',
+		method: ['POST'],
+		handler: async (_req, res) => {
+			const reqId = uuidv4();
+			const logData = { reqId };
+			logger.info(logData, '/github-bot request');
+
+			let bufferedReq: BufferedIncomingMessage;
+			let body: any;
+
+			// code based on https://github.com/slackapi/bolt-js/blob/0e827339a85804d868a7ddd7fdd4af9ec5b4e135/src/receivers/HTTPReceiver.ts#L416
+			try {
+				bufferedReq = await HTTPModuleFunctions.parseAndVerifyHTTPRequest(
+					{
+						enabled: false,
+						signingSecret: "",
+					},
+					_req,
+				);
+			} catch (err) {
+				const e = err as any;
+				logger.error({ errorMsg: `Failed to parse the request body: ${e.message}`, ...logData }, '/github-bot failed');
+				res.writeHead(401);
+				res.write({ err: JSON.stringify(e.message) });
+				res.end();
+				return
+			}
+
+			try {
+				body = HTTPModuleFunctions.parseHTTPRequestBody(bufferedReq);
+			} catch (err) {
+				const e = err as any;
+				logger.error({ errorMsg: `Malformed request body: ${e.message}`, ...logData }, '/github-bot failed');
+				res.writeHead(400);
+				res.write({ err: JSON.stringify(e.message) });
+				return;
+			}
+
+			try {
+				logger.info({ reqBody: body, ...logData }, '/github-bot processing');
+
+				res.writeHead(200);
+				res.write(JSON.stringify(body));
+				res.end();
+
+				logger.info(logData, '/github-bot response - OK');
+			} catch (err) {
+				const e = err as any;
+				logger.error({ errorMsg: `Request handling failed: ${e.message}`, ...logData }, '/github-bot failed');
+				res.writeHead(503);
+				res.write({ err: JSON.stringify(e.message) });
+				res.end();
+
+				return
+			}
+		}
+	},
 ];
+
