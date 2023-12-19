8d39c0ecc244f7b5717d2bf40eb863fedea598d2
Radu Lucuț <radulucut@pm.me>
2023-12-19 16:59:10 +0200

    Fix measurement ID issue (#54)
    * Updated docs and added examples on how to use probes from previous measurement
    
    * Remove lowercase conversion
    
    * Fix tests

--- packages/bot-utils/tests/measurements-request.test.ts
+++ packages/bot-utils/tests/measurements-request.test.ts
@@ -9,34 +9,41 @@ describe('Utils', () => {
 			it('should parse ping args', () => {
 				const args = 'ping google.com from New York';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'ping',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 1,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'ping',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 1,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {},
+					},
+				]);
 			});
 
 			it('should parse ping args with flags', () => {
 				const args = 'ping google.com from New York --limit 2 --packets 3';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'ping',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 2,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						packets: 3
-					}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'ping',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 2,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							packets: 3,
+						},
+					},
+				]);
 			});
 
 			it('should throw if incorrect ping flag', () => {
-				const args = 'ping google.com from New York --limit 2 --packets 3 --protocol icmp';
-				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow('Invalid option "protocol" for "ping"!\nExpected "packets, latency, target, from, limit, share".');
+				const args =
+					'ping google.com from New York --limit 2 --packets 3 --protocol icmp';
+				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow(
+					'Invalid option "protocol" for "ping"!\nExpected "packets, latency, target, from, limit, share".'
+				);
 			});
 		});
 
@@ -44,35 +51,43 @@ describe('Utils', () => {
 			it('should parse traceroute args', () => {
 				const args = 'traceroute google.com from New York';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'traceroute',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 1,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'traceroute',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 1,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {},
+					},
+				]);
 			});
 
 			it('should parse traceroute args with flags', () => {
-				const args = 'traceroute google.com from New York --limit 2 --protocol tcp --port 80';
+				const args =
+					'traceroute google.com from New York --limit 2 --protocol tcp --port 80';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'traceroute',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 2,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						protocol: 'TCP',
-						port: 80
-					}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'traceroute',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 2,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							protocol: 'TCP',
+							port: 80,
+						},
+					},
+				]);
 			});
 
 			it('should throw if incorrect traceroute flag', () => {
-				const args = 'traceroute google.com from New York --limit 2 --protocol tcp --port 80 --packets 3';
-				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow('Invalid option "packets" for "traceroute"!\nExpected "protocol, port, target, from, limit, share".');
+				const args =
+					'traceroute google.com from New York --limit 2 --protocol tcp --port 80 --packets 3';
+				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow(
+					'Invalid option "packets" for "traceroute"!\nExpected "protocol, port, target, from, limit, share".'
+				);
 			});
 		});
 
@@ -80,40 +95,48 @@ describe('Utils', () => {
 			it('should parse dns args', () => {
 				const args = 'dns google.com from New York';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'dns',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 1,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'dns',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 1,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {},
+					},
+				]);
 			});
 
 			it('should parse dns args with flags', () => {
-				const args = 'dns google.com from New York --limit 2 --query AAAA --protocol tcp --port 80 --resolver 1.1.1.1 --trace';
+				const args =
+					'dns google.com from New York --limit 2 --query AAAA --protocol tcp --port 80 --resolver 1.1.1.1 --trace';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'dns',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 2,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						query: {
-							type: 'AAAA'
+				expect(result).toEqual([
+					{
+						type: 'dns',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 2,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							query: {
+								type: 'AAAA',
+							},
+							protocol: 'TCP',
+							port: 80,
+							resolver: '1.1.1.1',
+							trace: true,
 						},
-						protocol: 'TCP',
-						port: 80,
-						resolver: '1.1.1.1',
-						trace: true,
-					}
-				}]);
+					},
+				]);
 			});
 
 			it('should throw if incorrect dns flag', () => {
-				const args = 'dns google.com from New York --limit 2 --query AAAA --protocol tcp --port 80 --test';
-				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow('Invalid option "test" for "dns"!\nExpected "query, protocol, port, resolver, trace, latency, target, from, limit, share".');
+				const args =
+					'dns google.com from New York --limit 2 --query AAAA --protocol tcp --port 80 --test';
+				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow(
+					'Invalid option "test" for "dns"!\nExpected "query, protocol, port, resolver, trace, latency, target, from, limit, share".'
+				);
 			});
 		});
 
@@ -121,36 +144,44 @@ describe('Utils', () => {
 			it('should parse mtr args', () => {
 				const args = 'mtr google.com from New York';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'mtr',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 1,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'mtr',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 1,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {},
+					},
+				]);
 			});
 
 			it('should parse mtr args with flags', () => {
-				const args = 'mtr google.com from New York --limit 2 --protocol tcp --port 80 --packets 16';
+				const args =
+					'mtr google.com from New York --limit 2 --protocol tcp --port 80 --packets 16';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'mtr',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 2,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						protocol: 'TCP',
-						port: 80,
-						packets: 16
-					}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'mtr',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 2,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							protocol: 'TCP',
+							port: 80,
+							packets: 16,
+						},
+					},
+				]);
 			});
 
 			it('should throw if incorrect mtr flag', () => {
-				const args = 'mtr google.com from New York --limit 2 --protocol tcp --port 80 --packets 16 --test';
-				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow('Invalid option "test" for "mtr"!\nExpected "protocol, port, packets, target, from, limit, share".');
+				const args =
+					'mtr google.com from New York --limit 2 --protocol tcp --port 80 --packets 16 --test';
+				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow(
+					'Invalid option "test" for "mtr"!\nExpected "protocol, port, packets, target, from, limit, share".'
+				);
 			});
 		});
 
@@ -158,80 +189,91 @@ describe('Utils', () => {
 			it('should parse http args', () => {
 				const args = 'http google.com from New York';
 				const result = buildPostMeasurements(argsToFlags(args));
-				expect(result).toEqual([{
-					type: 'http',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 1,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						protocol: 'HTTP',
-						request: {
-							host: 'google.com',
-							path: '/',
-							headers: {},
+				expect(result).toEqual([
+					{
+						type: 'http',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 1,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							protocol: 'HTTP',
+							request: {
+								host: 'google.com',
+								path: '/',
+								headers: {},
+							},
 						},
-					}
-				}]);
+					},
+				]);
 			});
 
 			it('should parse http args with flags', () => {
-				const args = 'http google.com from New York --limit 2 --path / --query ?a=abc --host google.com --method get --port 80 --protocol https --header "Content-Type: text/html; charset=utf-8" --header "Content-Encoding: gzip"';
+				const args =
+					'http google.com from New York --limit 2 --path / --query ?a=abc --host google.com --method get --port 80 --protocol https --header "Content-Type: text/html; charset=utf-8" --header "Content-Encoding: gzip"';
 				const result = buildPostMeasurements(argsToFlags(args));
 
-				expect(result).toEqual([{
-					type: 'http',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 2,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						port: 80,
-						protocol: 'HTTPS',
-						request: {
-							path: '/',
-							query: '?a=abc',
-							method: 'GET',
-							host: 'google.com',
-							headers: {
-								'Content-Encoding': 'gzip',
-								'Content-Type': 'text/html; charset=utf-8'
-							}
-						}
-					}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'http',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 2,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							port: 80,
+							protocol: 'HTTPS',
+							request: {
+								path: '/',
+								query: '?a=abc',
+								method: 'GET',
+								host: 'google.com',
+								headers: {
+									'Content-Encoding': 'gzip',
+									'Content-Type': 'text/html; charset=utf-8',
+								},
+							},
+						},
+					},
+				]);
 			});
 
 			it('should infer url for http parse', () => {
-				const args = 'http https://google.com:80/test?a=abc from New York --limit 2 --method get --header "Content-Encoding: gzip" --header "Content-Type: text/html; charset=utf-8"';
+				const args =
+					'http https://google.com:80/test?a=abc from New York --limit 2 --method get --header "Content-Encoding: gzip" --header "Content-Type: text/html; charset=utf-8"';
 				const result = buildPostMeasurements(argsToFlags(args));
 
-				expect(result).toEqual([{
-					type: 'http',
-					target: 'google.com',
-					inProgressUpdates: false,
-					limit: 2,
-					locations: [{ magic: 'new york' }],
-					measurementOptions: {
-						port: 80,
-						protocol: 'HTTPS',
-						request: {
-							path: '/test',
-							query: 'a=abc',
-							method: 'GET',
-							host: 'google.com',
-							headers: {
-								'Content-Encoding': 'gzip',
-								'Content-Type': 'text/html; charset=utf-8'
-							}
-						}
-					}
-				}]);
+				expect(result).toEqual([
+					{
+						type: 'http',
+						target: 'google.com',
+						inProgressUpdates: false,
+						limit: 2,
+						locations: [{ magic: 'New York' }],
+						measurementOptions: {
+							port: 80,
+							protocol: 'HTTPS',
+							request: {
+								path: '/test',
+								query: 'a=abc',
+								method: 'GET',
+								host: 'google.com',
+								headers: {
+									'Content-Encoding': 'gzip',
+									'Content-Type': 'text/html; charset=utf-8',
+								},
+							},
+						},
+					},
+				]);
 			});
 
 			it('should throw if incorrect http flag', () => {
-				const args = 'http google.com from New York --limit 2 --path / --query ?a=abc --host google.com --method get --port 80 --protocol https --header Content-Encoding: gzip --header Content-Type: text/html; charset=utf-8 --test';
-				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow('Invalid option "test" for "http"!\nExpected "protocol, port, resolver, method, path, query, host, header, latency, full, target, from, limit, share".');
+				const args =
+					'http google.com from New York --limit 2 --path / --query ?a=abc --host google.com --method get --port 80 --protocol https --header Content-Encoding: gzip --header Content-Type: text/html; charset=utf-8 --test';
+				expect(() => buildPostMeasurements(argsToFlags(args))).toThrow(
+					'Invalid option "test" for "http"!\nExpected "protocol, port, resolver, method, path, query, host, header, latency, full, target, from, limit, share".'
+				);
 			});
 		});
 	});
--- packages/bot-utils/tests/target-query.test.ts
+++ packages/bot-utils/tests/target-query.test.ts
@@ -1,94 +1,100 @@
 import { describe, expect, it } from 'vitest';
 
-import { findAndRemoveResolver, parseTargetQuery, TargetQuery } from '../src/target-query';
+import {
+	findAndRemoveResolver,
+	parseTargetQuery,
+	TargetQuery,
+} from '../src/target-query';
 
 describe('Utils', () => {
-    describe('parseTargetQuery', () => {
-        it('simple', () => {
-            const cmd = 'ping';
-            const args = ['example.com'];
+	describe('parseTargetQuery', () => {
+		it('simple', () => {
+			const cmd = 'ping';
+			const args = ['example.com'];
 
-            const q: TargetQuery = parseTargetQuery(cmd, args);
+			const q: TargetQuery = parseTargetQuery(cmd, args);
 
-            expect(q.target).toEqual('example.com');
-            expect(q.from).toEqual(undefined);
-        });
+			expect(q.target).toEqual('example.com');
+			expect(q.from).toEqual(undefined);
+		});
 
-        it('simple with resolver', () => {
-            const cmd = 'dns';
-            const args = ['example.com', '@1.1.1.1'];
+		it('simple with resolver', () => {
+			const cmd = 'dns';
+			const args = ['example.com', '@1.1.1.1'];
 
-            const q: TargetQuery = parseTargetQuery(cmd, args);
+			const q: TargetQuery = parseTargetQuery(cmd, args);
 
-            expect(q.target).toEqual('example.com');
-            expect(q.from).toEqual(undefined);
-            expect(q.resolver).toEqual('1.1.1.1');
-        });
+			expect(q.target).toEqual('example.com');
+			expect(q.from).toEqual(undefined);
+			expect(q.resolver).toEqual('1.1.1.1');
+		});
 
-        it('resolver not allowed', () => {
-            const cmd = 'ping';
-            const args = ['example.com', '@1.1.1.1'];
+		it('resolver not allowed', () => {
+			const cmd = 'ping';
+			const args = ['example.com', '@1.1.1.1'];
 
-            expect(() => parseTargetQuery(cmd, args)).toThrowError('does not accept a resolver argument');
-        });
+			expect(() => parseTargetQuery(cmd, args)).toThrowError(
+				'does not accept a resolver argument'
+			);
+		});
 
-        it('target from x', () => {
-            const cmd = 'ping';
-            const args = ['example.com', 'from', 'London'];
+		it('target from x', () => {
+			const cmd = 'ping';
+			const args = ['example.com', 'from', 'London'];
 
-            const q: TargetQuery = parseTargetQuery(cmd, args);
+			const q: TargetQuery = parseTargetQuery(cmd, args);
 
-            expect(q.target).toEqual('example.com');
-            expect(q.from).toEqual('london');
-        });
+			expect(q.target).toEqual('example.com');
+			expect(q.from).toEqual('London');
+		});
 
-        it('target from x with resolver', () => {
-            const cmd = 'http';
-            const args = ['example.com', 'from', 'London', '@1.1.1.1'];
+		it('target from x with resolver', () => {
+			const cmd = 'http';
+			const args = ['example.com', 'from', 'London', '@1.1.1.1'];
 
-            const q: TargetQuery = parseTargetQuery(cmd, args);
+			const q: TargetQuery = parseTargetQuery(cmd, args);
 
-            expect(q.target).toEqual('example.com');
-            expect(q.from).toEqual('london');
-            expect(q.resolver).toEqual('1.1.1.1');
-        });
-    });
+			expect(q.target).toEqual('example.com');
+			expect(q.from).toEqual('London');
+			expect(q.resolver).toEqual('1.1.1.1');
+		});
+	});
 
-    describe('findAndRemoveResolver', () => {
-        it('simple no resolver', () => {
-            const args = ['example.com'];
+	describe('findAndRemoveResolver', () => {
+		it('simple no resolver', () => {
+			const args = ['example.com'];
 
-            const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
+			const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
 
-            expect(resolver).toEqual('');
-            expect(argsWithoutResolver).toEqual(args);
-        });
+			expect(resolver).toEqual('');
+			expect(argsWithoutResolver).toEqual(args);
+		});
 
-        it('no resolver', () => {
-            const args = ['example.com', 'from', 'London'];
+		it('no resolver', () => {
+			const args = ['example.com', 'from', 'London'];
 
-            const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
+			const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
 
-            expect(resolver).toEqual('');
-            expect(argsWithoutResolver).toEqual(args);
-        });
+			expect(resolver).toEqual('');
+			expect(argsWithoutResolver).toEqual(args);
+		});
 
-        it('resolver and from', () => {
-            const args = ['example.com', '@1.1.1.1', 'from', 'London'];
+		it('resolver and from', () => {
+			const args = ['example.com', '@1.1.1.1', 'from', 'London'];
 
-            const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
+			const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
 
-            expect(resolver).toEqual('1.1.1.1');
-            expect(argsWithoutResolver).toEqual(['example.com', 'from', 'London']);
-        });
+			expect(resolver).toEqual('1.1.1.1');
+			expect(argsWithoutResolver).toEqual(['example.com', 'from', 'London']);
+		});
 
-        it('resolver only', () => {
-            const args = ['example.com', '@1.1.1.1'];
+		it('resolver only', () => {
+			const args = ['example.com', '@1.1.1.1'];
 
-            const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
+			const [resolver, argsWithoutResolver] = findAndRemoveResolver(args);
 
-            expect(resolver).toEqual('1.1.1.1');
-            expect(argsWithoutResolver).toEqual(['example.com']);
-        });
-    });
+			expect(resolver).toEqual('1.1.1.1');
+			expect(argsWithoutResolver).toEqual(['example.com']);
+		});
+	});
 });
