35ded7137f232213773fe6ed916470c17b69d8a0
Radu Lucuț <git@radulucut.com>
2024-10-28 14:30:21 +0200

    fix: request a new anonymous token if expired (#71)

--- packages/slack/src/tests/auth.test.ts
+++ packages/slack/src/tests/auth.test.ts
@@ -335,7 +335,7 @@ describe('Auth', () => {
 			expect(newToken).toEqual(null);
 		});
 
-		it('should request a new anonymous token', async () => {
+		it('should request a new anonymous token - no current token', async () => {
 			const now = new Date();
 			const installationId = 'I123';
 
@@ -363,8 +363,87 @@ describe('Auth', () => {
 
 			expect(installationStoreMock.updateToken).toHaveBeenCalledWith(
 				installationId,
-				expectedToken,
+				{
+					...expectedToken,
+					isAnonymous: true,
+				},
 			);
+
+			expect(fetchSpy).toHaveBeenCalledWith(`${config.authUrl}/oauth/token`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Content-Length': '107',
+				},
+				body: 'client_id=client_id&client_secret=client_secret&grant_type=globalping_client_credentials&scope=measurements',
+			});
+
+			expect(newToken).toEqual(expectedToken);
+		});
+
+		it('should request a new anonymous token - current anonymous token is expired', async () => {
+			const now = new Date();
+			const installationId = 'I123';
+
+			const currentToken: AuthToken = {
+				access_token: 'token',
+				refresh_token: 'refresh_token',
+				expires_in: 3600,
+				token_type: 'Bearer',
+				expiry: Math.floor(now.getTime() / 1000),
+				isAnonymous: true,
+			};
+
+			const expectedToken: AuthToken = {
+				access_token: 'new_tok3n',
+				refresh_token: 'new_refresh_tok3n',
+				expires_in: 3600,
+				token_type: 'Bearer',
+				expiry: Math.floor(now.getTime() / 1000 + 13600),
+			};
+
+			vi.spyOn(Date, 'now').mockReturnValue(now.getTime() + 1000000);
+
+			vi.spyOn(installationStoreMock, 'getToken').mockResolvedValue(currentToken);
+
+			const fetchSpy = vi.spyOn(global, 'fetch');
+			fetchSpy.mockResolvedValueOnce({
+				ok: true,
+				status: 400,
+				json: async () => ({
+					error: AuthorizeErrorType.InvalidGrant,
+					error_description: 'Invalid token',
+				}),
+			} as Response);
+
+			fetchSpy.mockResolvedValueOnce({
+				ok: true,
+				status: 200,
+				json: async () => expectedToken,
+			} as Response);
+
+			const newToken = await oauth.GetToken(installationId);
+
+			expect(installationStoreMock.getToken).toHaveBeenCalledWith(installationId);
+
+			expect(installationStoreMock.updateToken).toHaveBeenCalledWith(
+				installationId,
+				{
+					...expectedToken,
+					isAnonymous: true,
+				},
+			);
+
+			expect(fetchSpy).toHaveBeenCalledTimes(2);
+
+			expect(fetchSpy).toHaveBeenCalledWith(`${config.authUrl}/oauth/token`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+					'Content-Length': '100',
+				},
+				body: 'client_id=client_id&client_secret=client_secret&refresh_token=refresh_token&grant_type=refresh_token',
+			});
 
 			expect(fetchSpy).toHaveBeenCalledWith(`${config.authUrl}/oauth/token`, {
 				method: 'POST',
