a89d3d415bac66d5a2246275373a0d1debd7d561
Radu Lucuț <git@radulucut.com>
2024-10-29 15:01:46 +0200

    fix: persist anonymous token after auth login and uninstall (#73)
    * fix: persist anonymous token after auth login and uninstall

---
+++ packages/slack/migrations/202410291200_installations_add_installation_token.ts
@@ -0,0 +1,42 @@
+import { Knex } from 'knex';
+
+import { Tables } from '../src/db.js';
+import { AuthToken } from '@globalping/bot-utils';
+
+export const up = async (knex: Knex) => {
+	console.log('Adding installation_token columns to installations table');
+
+	await knex.schema.table(Tables.Installations, (table) => {
+		table.json('installation_token');
+	});
+
+	const rows = await knex(Tables.Installations).select('id', 'token');
+
+	for (const row of rows) {
+		const token = row.token
+			? (JSON.parse(row.token as unknown as string) as AuthToken)
+			: null;
+
+		if (token?.isAnonymous) {
+			const update: {
+				token?: string | null;
+				installation_token?: string | null;
+			} = {};
+			update.token = null;
+			update.installation_token = JSON.stringify(token);
+
+			await knex
+				.table(Tables.Installations)
+				.where('id', row.id)
+				.update(update as never);
+		}
+	}
+};
+
+export const down = async (knex: Knex) => {
+	console.log('Dropping installation_token columns from installations table');
+
+	await knex.schema.table(Tables.Installations, (table) => {
+		table.dropColumn('installation_token');
+	});
+};
