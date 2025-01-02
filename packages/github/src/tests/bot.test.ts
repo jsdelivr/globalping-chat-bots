c33cb840ca4db28d53498871defec9deb5b453d0
Radu Lucuț <git@radulucut.com>
2025-01-02 20:39:42 +0200

    misc: github - add tests

--- packages/github/src/tests/bot.test.ts
+++ packages/github/src/tests/bot.test.ts
@@ -1,5 +1,6 @@
-import { describe, expect, it } from 'vitest';
+import { afterEach, describe, expect, it, vi } from 'vitest';
 import {
+	Bot,
 	cleanUpCommandText,
 	isMentionNotification,
 	parseCommandfromMention,
@@ -7,9 +8,386 @@ import {
 	splitMessageFooter,
 } from '../bot.js';
 
-import { GithubTargetType } from '../types.js';
+import {
+	Config,
+	GithubNotificationRequest,
+	GithubTargetType,
+} from '../types.js';
+import {
+	getDefaultDnsResponse,
+	getDefaultHttpResponse,
+	getDefaultMtrResponse,
+	getDefaultPingResponse,
+	getDefaultTracerouteResponse,
+	mockGetMeasurement,
+	mockGithubClient,
+	mockIncomingMessage,
+	mockLogger,
+	mockPostMeasurement,
+} from './utils.js';
 
-describe('Mention', () => {
+describe('Bot', () => {
+	afterEach(() => {
+		vi.resetAllMocks();
+	});
+
+	const loggerMock = mockLogger();
+	const githubClientMock = mockGithubClient();
+	const postMeasurementMock = mockPostMeasurement();
+	const getMeasurementMock = mockGetMeasurement();
+	const configMock: Config = {
+		globalpingToken: 'globalping_token',
+		githubPersonalAccessToken: 'access_token',
+		githubBotApiKey: 'api_key',
+		githubBotHandle: 'globalping',
+	};
+
+	const bot = new Bot(
+		configMock,
+		loggerMock,
+		githubClientMock,
+		postMeasurementMock,
+		getMeasurementMock,
+	);
+
+	describe('HandleRequest', () => {
+		it('should succesfully handle the command - ping', async () => {
+			const githubRequest: GithubNotificationRequest = {
+				subject: '',
+				bodyPlain: `@globalping ping google.com
+
+--
+Reply to this email directly or view it on GitHub:
+https://github.com/myuser/myrepo/issues/1
+You are receiving this because you were mentioned.
+
+Message ID: <myuser/myrepo/issues/1@github.com>`,
+			};
+
+			postMeasurementMock.mockResolvedValue({
+				id: 'm345ur3m3nt',
+				probesCount: 1,
+			});
+
+			const expectedResponse = getDefaultPingResponse();
+			getMeasurementMock.mockResolvedValue(expectedResponse);
+
+			const req = mockIncomingMessage(
+				{
+					headers: {
+						'api-key': configMock.githubBotApiKey,
+					},
+				},
+				Buffer.from(JSON.stringify(githubRequest)),
+			);
+
+			const res = {
+				writeHead: vi.fn(),
+				write: vi.fn(),
+				end: vi.fn(),
+			} as any;
+
+			await bot.HandleRequest(req, res);
+
+			expect(postMeasurementMock).toHaveBeenCalledWith(
+				{
+					type: 'ping',
+					target: 'google.com',
+					inProgressUpdates: false,
+					limit: 1,
+					locations: [{ magic: 'world' }],
+					measurementOptions: {},
+				},
+				'globalping_token',
+			);
+
+			expect(getMeasurementMock).toHaveBeenCalledWith('m345ur3m3nt');
+
+			expect(githubClientMock.rest.issues.createComment).toHaveBeenCalledWith({
+				owner: 'myuser',
+				repo: 'myrepo',
+				issue_number: 1,
+				body: `Here are the results for \`ping google.com\`
+>**Amsterdam, NL, EU, Gigahost AS (AS56655)**
+\`\`\`
+${expectedResponse.results[0].result.rawOutput}
+\`\`\`
+`,
+			});
+		});
+
+		it('should succesfully handle the command - dns', async () => {
+			const githubRequest: GithubNotificationRequest = {
+				subject: '',
+				bodyPlain: `@globalping dns google.com from Berlin --resolver 1.1.1.1
+
+--
+Reply to this email directly or view it on GitHub:
+https://github.com/myuser/myrepo/issues/1
+You are receiving this because you were mentioned.
+
+Message ID: <myuser/myrepo/issues/1@github.com>`,
+			};
+
+			postMeasurementMock.mockResolvedValue({
+				id: 'm345ur3m3nt',
+				probesCount: 1,
+			});
+
+			const expectedResponse = getDefaultDnsResponse();
+			getMeasurementMock.mockResolvedValue(expectedResponse);
+
+			const req = mockIncomingMessage(
+				{
+					headers: {
+						'api-key': configMock.githubBotApiKey,
+					},
+				},
+				Buffer.from(JSON.stringify(githubRequest)),
+			);
+
+			const res = {
+				writeHead: vi.fn(),
+				write: vi.fn(),
+				end: vi.fn(),
+			} as any;
+
+			await bot.HandleRequest(req, res);
+
+			expect(postMeasurementMock).toHaveBeenCalledWith(
+				{
+					type: 'dns',
+					target: 'google.com',
+					inProgressUpdates: false,
+					limit: 1,
+					locations: [{ magic: 'Berlin' }],
+					measurementOptions: {
+						resolver: '1.1.1.1',
+					},
+				},
+				'globalping_token',
+			);
+
+			expect(getMeasurementMock).toHaveBeenCalledWith('m345ur3m3nt');
+
+			expect(githubClientMock.rest.issues.createComment).toHaveBeenCalledWith({
+				owner: 'myuser',
+				repo: 'myrepo',
+				issue_number: 1,
+				body: `Here are the results for \`dns google.com from Berlin --resolver 1.1.1.1\`
+>**Helsinki, FI, EU, Hetzner Online GmbH (AS24940)**
+\`\`\`
+${expectedResponse.results[0].result.rawOutput.trim()}
+\`\`\`
+`,
+			});
+		});
+
+		it('should succesfully handle the command - http', async () => {
+			const githubRequest: GithubNotificationRequest = {
+				subject: '',
+				bodyPlain: `@globalping http jsdelivr.com --host www.jsdelivr.com --protocol https --port 443 --path "/package/npm/test" --query "nav=stats"
+
+--
+Reply to this email directly or view it on GitHub:
+https://github.com/myuser/myrepo/issues/1
+You are receiving this because you were mentioned.
+
+Message ID: <myuser/myrepo/issues/1@github.com>`,
+			};
+
+			postMeasurementMock.mockResolvedValue({
+				id: 'm345ur3m3nt',
+				probesCount: 1,
+			});
+
+			const expectedResponse = getDefaultHttpResponse();
+			getMeasurementMock.mockResolvedValue(expectedResponse);
+
+			const req = mockIncomingMessage(
+				{
+					headers: {
+						'api-key': configMock.githubBotApiKey,
+					},
+				},
+				Buffer.from(JSON.stringify(githubRequest)),
+			);
+
+			const res = {
+				writeHead: vi.fn(),
+				write: vi.fn(),
+				end: vi.fn(),
+			} as any;
+
+			await bot.HandleRequest(req, res);
+
+			expect(postMeasurementMock).toHaveBeenCalledWith(
+				{
+					type: 'http',
+					target: 'jsdelivr.com',
+					inProgressUpdates: false,
+					limit: 1,
+					locations: [{ magic: 'world' }],
+					measurementOptions: {
+						port: 443,
+						protocol: 'HTTPS',
+						request: {
+							headers: {},
+							host: 'www.jsdelivr.com',
+							path: '/package/npm/test',
+							query: 'nav=stats',
+						},
+					},
+				},
+				'globalping_token',
+			);
+
+			expect(getMeasurementMock).toHaveBeenCalledWith('m345ur3m3nt');
+
+			expect(githubClientMock.rest.issues.createComment).toHaveBeenCalledWith({
+				owner: 'myuser',
+				repo: 'myrepo',
+				issue_number: 1,
+				body: `Here are the results for \`http jsdelivr.com --host www.jsdelivr.com --protocol https --port 443 --path "/package/npm/test" --query "nav=stats"\`
+>**Chisinau, MD, EU, STARK INDUSTRIES SOLUTIONS LTD (AS44477)**
+\`\`\`
+${expectedResponse.results[0].result.rawOutput}
+\`\`\`
+`,
+			});
+		});
+
+		it('should succesfully handle the command - mtr', async () => {
+			const githubRequest: GithubNotificationRequest = {
+				subject: '',
+				bodyPlain: `@globalping mtr google.com
+
+--
+Reply to this email directly or view it on GitHub:
+https://github.com/myuser/myrepo/issues/1
+You are receiving this because you were mentioned.
+
+Message ID: <myuser/myrepo/issues/1@github.com>`,
+			};
+
+			postMeasurementMock.mockResolvedValue({
+				id: 'm345ur3m3nt',
+				probesCount: 1,
+			});
+
+			const expectedResponse = getDefaultMtrResponse();
+			getMeasurementMock.mockResolvedValue(expectedResponse);
+
+			const req = mockIncomingMessage(
+				{
+					headers: {
+						'api-key': configMock.githubBotApiKey,
+					},
+				},
+				Buffer.from(JSON.stringify(githubRequest)),
+			);
+
+			const res = {
+				writeHead: vi.fn(),
+				write: vi.fn(),
+				end: vi.fn(),
+			} as any;
+
+			await bot.HandleRequest(req, res);
+
+			expect(postMeasurementMock).toHaveBeenCalledWith(
+				{
+					type: 'mtr',
+					target: 'google.com',
+					inProgressUpdates: false,
+					limit: 1,
+					locations: [{ magic: 'world' }],
+					measurementOptions: {},
+				},
+				'globalping_token',
+			);
+
+			expect(getMeasurementMock).toHaveBeenCalledWith('m345ur3m3nt');
+
+			expect(githubClientMock.rest.issues.createComment).toHaveBeenCalledWith({
+				owner: 'myuser',
+				repo: 'myrepo',
+				issue_number: 1,
+				body: `Here are the results for \`mtr google.com\`
+>**Beauharnois, CA, NA, OVH SAS (AS16276)**
+\`\`\`
+${expectedResponse.results[0].result.rawOutput.trim()}
+\`\`\`
+`,
+			});
+		});
+
+		it('should succesfully handle the command - traceroute', async () => {
+			const githubRequest: GithubNotificationRequest = {
+				subject: '',
+				bodyPlain: `@globalping traceroute google.com
+
+--
+Reply to this email directly or view it on GitHub:
+https://github.com/myuser/myrepo/issues/1
+You are receiving this because you were mentioned.
+
+Message ID: <myuser/myrepo/issues/1@github.com>`,
+			};
+
+			postMeasurementMock.mockResolvedValue({
+				id: 'm345ur3m3nt',
+				probesCount: 1,
+			});
+
+			const expectedResponse = getDefaultTracerouteResponse();
+			getMeasurementMock.mockResolvedValue(expectedResponse);
+
+			const req = mockIncomingMessage(
+				{
+					headers: {
+						'api-key': configMock.githubBotApiKey,
+					},
+				},
+				Buffer.from(JSON.stringify(githubRequest)),
+			);
+
+			const res = {
+				writeHead: vi.fn(),
+				write: vi.fn(),
+				end: vi.fn(),
+			} as any;
+
+			await bot.HandleRequest(req, res);
+
+			expect(postMeasurementMock).toHaveBeenCalledWith(
+				{
+					type: 'traceroute',
+					target: 'google.com',
+					inProgressUpdates: false,
+					limit: 1,
+					locations: [{ magic: 'world' }],
+					measurementOptions: {},
+				},
+				'globalping_token',
+			);
+
+			expect(getMeasurementMock).toHaveBeenCalledWith('m345ur3m3nt');
+
+			expect(githubClientMock.rest.issues.createComment).toHaveBeenCalledWith({
+				owner: 'myuser',
+				repo: 'myrepo',
+				issue_number: 1,
+				body: `Here are the results for \`traceroute google.com\`
+>**Rotterdam, NL, EU, DELTA Fiber Nederland B.V. (AS15435)**
+\`\`\`
+${expectedResponse.results[0].result.rawOutput.trim()}
+\`\`\`
+`,
+			});
+		});
+	});
+
 	describe('parseCommandfromMention', () => {
 		it('valid ', () => {
 			const text
---
+++ packages/github/src/tests/utils.ts
@@ -0,0 +1,59 @@
+import { vi } from 'vitest';
+
+import { Logger, Measurement } from '@globalping/bot-utils';
+import probeData from '../../../bot-utils/tests/mocks/probedata.json';
+import { Octokit } from 'octokit';
+import { Readable } from 'node:stream';
+import { IncomingMessage } from 'node:http';
+
+export const mockLogger = (): Logger => ({
+	info: vi.fn(),
+	error: vi.fn(),
+	debug: vi.fn(),
+}) as any;
+
+export const mockGithubClient = (): Octokit => ({
+	rest: {
+		issues: {
+			createComment: vi.fn(),
+		},
+	},
+}) as any;
+
+export const mockPostMeasurement = () => vi.fn();
+export const mockGetMeasurement = () => vi.fn();
+
+export const getDefaultDnsResponse = (): Measurement => {
+	return probeData.dns1 as any;
+};
+
+export const getDefaultHttpResponse = (): Measurement => {
+	return probeData.http1 as any;
+};
+
+export const getDefaultMtrResponse = (): Measurement => {
+	return probeData.mtr1 as any;
+};
+
+export const getDefaultPingResponse = (): Measurement => {
+	return probeData.ping1 as any;
+};
+
+export const getDefaultTracerouteResponse = (): Measurement => {
+	return probeData.traceroute1 as any;
+};
+
+export const mockIncomingMessage = (
+	options = {},
+	data: string | Buffer,
+): IncomingMessage => {
+	const readable = new Readable();
+	readable._read = () => {
+		readable.emit('data', data);
+		readable.emit('end');
+	};
+
+	const mockRequest = Object.assign(readable, options);
+
+	return mockRequest as IncomingMessage;
+};
